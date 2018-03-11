8a4a5f14d068cfc67539fc976491fb5ac0c1c7fa
Richard Capraro <richard.capraro@gmail.com>
2018-03-11 21:37:32 +0100

    Add operator overloading and lambda extension

---
+++ src/com/sada/learning/workshop5/2_LambdaExtensions.kt
@@ -0,0 +1,48 @@
+package com.sada.learning.workshop5
+
+class Request(val method: String, val query: String, val contentType: String)
+
+//class Response(var contents: String, var status: Int)
+//class Response(var contents: String, var status: Status)
+
+class Response(var contents: String, var status: Status) {
+    fun status(status: Status.() -> Unit) {
+
+    }
+}
+
+class Status(var code: Int, var description: String)
+
+class RouteHandler(val request: Request, val response: Response) {
+    var executeNext = false
+
+    fun next() {
+        executeNext = true
+    }
+}
+
+//higher order function which takes an extension function
+fun routeHandler(path: String, f: RouteHandler.() -> Unit): RouteHandler.() -> Unit = f
+
+fun response(response: Response.() -> Unit) {}
+
+fun main(args: Array<String>) {
+    routeHandler("/index.html") {
+        //get access to request because extension functions have access to properties of the class they are extending
+        if (request.query != "") {
+            //request.query
+            //process
+        }
+        /*response.status = 404
+        response.contents = "Not found"*/
+        // response.status.code = 404
+        response {
+            //get access to response properties
+            status {
+                code = 404
+                description = "Not found"
+            }
+        }
+
+    }
+}

a6aa18b444a57a71507b0e0cbad8ee8301fd6914
Richard Capraro <richard.capraro@gmail.com>
2018-03-13 22:07:37 +0100

    refactoring

--- src/com/sada/learning/workshop5/2_LambdaExtensions.kt
+++ src/com/sada/learning/workshop5/2_LambdaExtensions.kt
@@ -1,11 +1,19 @@
 package com.sada.learning.workshop5
 
-class Request(val method: String, val query: String, val contentType: String)
+class Request(val method: String, val query: String, val contentType: String) {
+    fun validate() {
+
+    }
+}
 
 //class Response(var contents: String, var status: Int)
 //class Response(var contents: String, var status: Status)
 
 class Response(var contents: String, var status: Status) {
+    fun content(content: String.() -> Unit) {
+
+    }
+
     fun status(status: Status.() -> Unit) {
 
     }
@@ -13,16 +21,10 @@ class Response(var contents: String, var status: Status) {
 
 class Status(var code: Int, var description: String)
 
-class RouteHandler(val request: Request, val response: Response) {
-    var executeNext = false
-
-    fun next() {
-        executeNext = true
-    }
-}
+class RouteHandler(val request: Request, val response: Response)
 
 //higher order function which takes an extension function
-fun routeHandler(path: String, f: RouteHandler.() -> Unit): RouteHandler.() -> Unit = f
+fun routeHandler(path: String, f: RouteHandler.() -> Unit) {}
 
 fun response(response: Response.() -> Unit) {}
 
@@ -31,12 +33,15 @@ fun main(args: Array<String>) {
         //get access to request because extension functions have access to properties of the class they are extending
         if (request.query != "") {
             //request.query
-            //process
+            request.validate()
         }
         /*response.status = 404
         response.contents = "Not found"*/
         // response.status.code = 404
         response {
+            content {
+                "this is a content"
+            }
             //get access to response properties
             status {
                 code = 404
